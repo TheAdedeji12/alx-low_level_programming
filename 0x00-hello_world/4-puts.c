19d805ec284d302c781f623129d50644938ac2c5
TheAdedeji12 <adedejiadejumo1212@gmail.com>
2023-09-07 11:34:40 -0700

    a C program that prints exactly :Programming is like a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Retun: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	retun(0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }

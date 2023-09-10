6a3492029b636cf26ff5a19f513e1d84f38ae983
TheAdedeji12 <adedejiadejumo1212@gmail.com>
2023-09-10 03:34:52 -0700

    this program prints the base 10 number , starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description - prints number from 0-9
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}

9300a644453c48a109bf831c5191ed7631911198
TheAdedeji12 <adedejiadejumo1212@gmail.com>
2023-09-10 05:07:36 -0700

    prints all combination of two digit number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
+	for (i = 0 ; i <= 9 ; i++)
+		putchar(i + '0');
+	return (0);
+}

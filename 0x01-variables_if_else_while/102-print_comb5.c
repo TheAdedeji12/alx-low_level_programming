6c5f8fe53766f41e1891619564b331a0b2f81ac9
TheAdedeji12 <adedejiadejumo1212@gmail.com>
2023-09-11 16:38:49 -0700

    prints all possible combinations of two two numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,8 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 10 ; i <= 99 ; i++)
-	for (j = i ; j <= 99 ; j++)
-	if (i != j)
+	for (i = 0 ; i <= 99 ; i++)
+	for (j = i + 1 ; j <= 99 ; j++)
 	{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');

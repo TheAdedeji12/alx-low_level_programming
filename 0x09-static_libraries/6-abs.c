f26669c7067eab2d39d16610fa9b430701956fc7
TheAdedeji12 <adedejiadejumo1212@gmail.com>
2023-10-03 07:53:58 -0700

    I have created a static library with all my .c files in it , then i wrote a script with the command on how to create the static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of a number
+ * @a: function parameter
+ * Return: -a or a;
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else if (a >= 0)
+	{
+		return (0);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+this is my static library repository, all my .c function file can be found in this directory
